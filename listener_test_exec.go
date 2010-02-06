c18f474bc32112d17b0bb90e80f2510bc97e0698
Jesse Dailey <jldailey@empi.re>
2010-02-06 06:23:05 +0000

    added flags support so you can use this program to test with webservers

--- listener_test_exec.go
+++ listener_test_exec.go
@@ -4,13 +4,18 @@ import (
 	"http"
 	"http/fcgi"
 	"io"
+	"flag"
 )
+
+var net *string = flag.String("net", "exec", "What type of network to listen on.")
+var bind *string = flag.String("b", "", "The address (or path) of the socket to bind to.")
 
 func HelloServer(con *http.Conn, req *http.Request) {
 	io.WriteString(con, "hello, world\r\n")
 }
 
 func main() {
+	flag.Parse()
 	// the hello world test from the http docs
 	http.Handle("/hello/", http.HandlerFunc(HelloServer))
 	// for testing response status codes
@@ -22,9 +27,7 @@ func main() {
 	}))
 	// for testing the serving of static files
 	http.Handle("/static/", http.FileServer("/tmp", "/static"))
-	// this process will be a FastCGI responder, accepting connections on stdin
-	// so we have to give it the same handlers as the fcgiMux from the test
-	if fdlisten, err := fcgi.Listen("exec", ""); err == nil {
-		http.Serve(fdlisten, nil)
+	if listen, err := fcgi.Listen(*net, *bind); err == nil {
+		http.Serve(listen, nil)
 	}
 }
